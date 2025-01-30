b5e14d457cecb31727966092880ff55ef97aee1d
Ilia Munaev <imunaev-@c1r6p3.hive.fi>
2025-01-30 11:53:08 +0200

    Feat add render_sprite()

--- src/render_game.c
+++ src/render_game.c
@@ -6,26 +6,12 @@
 /*   By: imunaev- <imunaev-@studen.hive.fi>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 16:15:33 by imunaev-          #+#    #+#             */
-/*   Updated: 2025/01/29 18:23:32 by imunaev-         ###   ########.fr       */
+/*   Updated: 2025/01/30 11:51:54 by imunaev-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 	
-bool 	render_player(t_game *game)
-{
-	if (mlx_image_to_window(game->mlx, game->sprites->player, 0, 0) == -1)
-	{
-		mlx_close_window(game->mlx);
-		ft_putstr_fd("ERROR: mlx_image_to_window\n", 2);
-		return(false);
-	}
-	else 
-	{
-		ft_putstr_fd("SUCCESS: render_game(): mlx_image_to_window -> ok\n", 2);  // test
-	}	
-	return (true);
-}
 /*
 void	flood_fill(t_game *game, char target, int row, int col)
 {
@@ -38,51 +24,34 @@ void	flood_fill(t_game *game, char target, int row, int col)
 	flood_fill(game, target, row, col - 1);
 }
 */
-bool 	render_grass(t_game *game)
-{
-	//flood_fill();
 
-	
-	if (mlx_image_to_window(game->mlx, game->sprites->grass, 0, 0) == -1)
+static bool	render_sprite(t_game *game, mlx_image_t *sprite)
+{
+	if (mlx_image_to_window(game->mlx, sprite, 0, 0) == -1)
 	{
 		mlx_close_window(game->mlx);
-		ft_putstr_fd("ERROR: mlx_image_to_window\n", 2);
+		ft_putstr_fd("ERROR: render_sprite failure\n", 2);
 		return(false);
-	}
-	else 
-	{
-		ft_putstr_fd("SUCCESS: render_game(): mlx_image_to_window -> ok\n", 2);  // test
-	}
-	
+	}	
 	return (true);
 }
+
 bool	render_game(t_game *game)
 {	
 	if (!game->sprites)
 	{
 		ft_putstr_fd("ERROR: render_game(): game->sprites is NULL\n", 2);
 		return (false);
-	}
-	else
-		ft_putstr_fd("SUCCESS: render_game(): game->sprites -> ok\n", 2);
+	}	
+	//flood_fill();
+
+	render_sprite(game, game->sprites->grass);
+	render_sprite(game, game->sprites->player);
 	
-	if(!render_grass(game))
-	{
-		ft_putstr_fd("ERROR: render_game(): render_grass() fail\n", 2);
-		return (false);
-	}
-	else 
-		ft_putstr_fd("SUCCESS: render_game(): render_grass() -> ok\n", 2);
-		
-	
-	if(!render_player(game))
-	{
-		ft_putstr_fd("ERROR: render_game(): render_player() fail\n", 2);
-		return (false);
-	}
-	else 
-		ft_putstr_fd("SUCCESS: render_game(): render_player() -> ok\n", 2);
-		
+	// render_sprite(game, '1');
+	// render_sprite(game, 'C');
+	// render_sprite(game, 'E');
+
 
 
 	return (true);
